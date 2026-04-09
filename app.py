6f4dad4bafd12cc69a4f6af4edb95e9a0bcb1b8c
Agam Raj <153552632+agamraj123@users.noreply.github.com>
2026-04-09 08:36:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,25 +2,19 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-# Load trained model
 model = joblib.load("payment_success_model.pkl")
 
-# App Title
-st.title("💳 Payment Success Analyzer")
+st.title("Payment Success Analyzer")
 
-st.write("Enter transaction amount to predict payment success")
-
-# Input from user
 amount = st.number_input("Transaction Amount (INR)", min_value=0.0)
 
-# Prediction
-if st.button("Predict Payment Status"):
+if st.button("Predict"):
 
     input_data = pd.DataFrame([[amount]], columns=["Amount (INR)"])
 
     prediction = model.predict(input_data)
 
     if prediction[0] == 1:
-        st.success("✅ Payment will likely SUCCESS")
+        st.success("Payment will likely SUCCESS")
     else:
-        st.error("❌ Payment may FAIL")
+        st.error("Payment may FAIL")
